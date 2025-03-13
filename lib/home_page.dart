134637f3a0c2b91eac3585a0b257ea5db0b8dabd
hnawn41 <aliouihanane41@gmail.com>
2025-03-13 03:09:46 +0100

    third

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,50 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 98,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                width: 338,
+                height: 48,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: Color(0xFF3C6FE2),
+                    width: 1,
+                  ),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search",
+                    hintStyle: TextStyle(color: Color(0xFF3C6FE2)),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 15, right: 10),
+                      child: SizedBox(
+                        width: 18, // 👈 Ensures icon has fixed size
+                        height: 18,
+                        child: Image.asset(
+                          'assets/search.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
